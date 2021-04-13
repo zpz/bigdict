bba3ab4a871e20c4eceb0b9fe34522a73f6091df
Zepu Zhang <zepu.zhang@gmail.com>
2021-04-13 00:09:45 -0700

    minor

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 README = (HERE / 'README.md').read_text()
 
+version = next(open(HERE / 'src/bigdict/__init__.py'))
+version = version.strip('\n').split('=')[1].strip(" '")
 
 setup(
     name='bigdict',
-    version='21.4.11',
+    version=version,
     description='A persisted, out-of-memory dict for Python',
     long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/zpz/bigdict',
-    author='Zepu Zhang',
-    author_email='zepu.zhang@gmail.com',
+    author='zpz',
     license='MIT',
     python_requires='>=3.7',
     package_dir={'': 'src'},
